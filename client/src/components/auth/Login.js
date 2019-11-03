e35976b64472e18e34358826b864cfb59b7e35ea
giordifungula <jodi.fungula@gmail.com>
2019-11-03 19:31:33 +0200

    linked redux to the login and register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -13,6 +13,12 @@ class Login extends Component {
       errors: {}
     };
   }
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
 componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
       this.props.history.push("/dashboard"); // push user to dashboard when they login
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -15,6 +15,12 @@ class Register extends Component {
       errors: {}
     };
   }
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
