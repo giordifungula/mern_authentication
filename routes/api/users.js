90d317313ce2cad1962f975b1e46354fe4d12a54
giordifungula <jodi.fungula@gmail.com>
2019-11-03 02:58:22 +0200

    added the login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -39,3 +39,54 @@ router.post("/register", (req,res)=> {
     })
 
 })
+
+router.post("/login", (req,res) => {
+    const {errors, isValid} = validateLoginInput(req.body);
+
+    // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+// Find user by email
+  User.findOne({ email }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User matched
+          // Create JWT Payload
+          const payload = {
+            id: user.id,
+            name: user.name
+          };
+  // Sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            {
+              expiresIn: 31556926 // 1 year in seconds
+            },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        } else {
+          return res
+            .status(400)
+            .json({ passwordincorrect: "Password incorrect" });
+        }
+      });
+    });
+    
+})
+
+module.exports = router;
